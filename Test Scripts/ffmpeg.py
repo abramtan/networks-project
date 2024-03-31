9fc334f2a38b516a571a7e071b9f6d10395917f1
Abram Tan <72917689+abramtan@users.noreply.github.com>
2024-03-31 23:40:18 +0800

    Update ffmpeg test

--- Test Scripts/ffmpeg.py
+++ Test Scripts/ffmpeg.py
@@ -1,9 +1,20 @@
+import ffmpeg_streaming
 from ffmpeg_streaming import Formats, Bitrate, Representation, Size
+import sys
+import datetime
 
-video = ffmpeg_streaming.input("BigBuckBunny.mp4")
+video = ffmpeg_streaming.input('/home/ubuntu/networks_project/BigBuckBunny.mp4')
+
+_144p  = Representation(Size(256, 144), Bitrate(95 * 1024, 64 * 1024)) # 144p alone uses almost 100% CPU
+
+def monitor(ffmpeg, duration, time_, time_left, process):
+    per = round(time_ / duration * 100)
+    sys.stdout.write(
+        "\rTranscoding...(%s%%) %s left [%s%s]" %
+        (per, datetime.timedelta(seconds=int(time_left)), '#' * per, '-' * (100 - per))
+    )
+    sys.stdout.flush()
+
 dash = video.dash(Formats.h264())
-dash.auto_generate_representations()
-_144p  = Representation(Size(256, 144), Bitrate(95 * 1024, 64 * 1024))
-_240p  = Representation(Size(426, 240), Bitrate(150 * 1024, 94 * 1024))
-dash.representations(_144p, _240p)
-dash.output('BigBuckBunny.mpd')
+dash.representations(_144p)
+dash.output('/home/ubuntu/networks_project/dash/test-1/BigBuckBunny.mpd', monitor=monitor)
