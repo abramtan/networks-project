5914144dcebdd5ca9ee20da8de248ee3a7eb109c
Abram Tan <72917689+abramtan@users.noreply.github.com>
2024-04-02 23:09:08 +0800

    Update ffmpeg to hls

--- Test Scripts/ffmpeg.py
+++ Test Scripts/ffmpeg.py
@@ -1,12 +1,9 @@
 import ffmpeg_streaming
-from ffmpeg_streaming import Formats, Bitrate, Representation, Size
+from ffmpeg_streaming import Formats
 import sys
 import datetime
 
-video = ffmpeg_streaming.input('/home/ubuntu/networks_project/media/dash/BigBuckBunny-short.mp4')
-
-_144p  = Representation(Size(256, 144), Bitrate(95 * 1024, 64 * 1024)) # 144p alone uses almost 100% CPU
-_240p  = Representation(Size(426, 240), Bitrate(150 * 1024, 94 * 1024))
+video = ffmpeg_streaming.input('./BigBuckBunny-short.mp4')
 
 def monitor(ffmpeg, duration, time_, time_left, process):
     per = round(time_ / duration * 100)
@@ -16,6 +13,6 @@ def monitor(ffmpeg, duration, time_, time_left, process):
     )
     sys.stdout.flush()
 
-dash = video.dash(Formats.h264())
-dash.representations(_144p, _240p)
-dash.output('/home/ubuntu/networks_project/media/dash/test-3/BigBuckBunny.mpd', monitor=monitor)
+hls = video.hls(Formats.h264())
+hls.auto_generate_representations([240, 144])
+hls.output('./dash/test-9/BigBuckBunny.m3u8', monitor=monitor)
